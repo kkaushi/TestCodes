244acee119e820a4e45940fd1ee5b767080b9fab
Ubuntu_32Bit <2kuldeepkaushik@gmail.com>
2014-07-26 16:33:01 -0700

    changes completed

--- StringGroups.c
+++ StringGroups.c
@@ -8,22 +8,25 @@
 
 void main()
 {
+	//Strings pool
 	char s1[]="This ";
 	char s2[]="is ";
 	char s3[]="Amerika ";
 	
-	char* ptr[3], *strs[2];
-	int i,j,indx[2]={0,0};
+	char* ptr[3], *strs[3];
+	int i,j,indx[3]={0,0};
 	
 	ptr[0]=s1;
 	ptr[1]=s2;
 	ptr[2]=s3;
 	
+	//Strings group	
 	strs[0]=s1;
 	strs[1]=s2;
+	strs[2]="Fake";
 	
 	
-	for(i=0;i<2;i++)
+	for(i=0;i<3;i++)
 	{
 		for(j=0;j<3;j++)
 		{
@@ -33,6 +36,6 @@ void main()
 			}
 		}
 	}
-	printf("Indx array is:%d, %d\n", indx[0], indx[1]);		
+	printf("Key - String present: 1, Not present: 0\nString1: %d\tString2: %d\tString3: %d\n", indx[0], indx[1], indx[2]);		
 }
 
