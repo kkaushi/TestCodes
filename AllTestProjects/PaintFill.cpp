04458bb9f71a2e61fb0c8b8f1f9c13dc0ef0bbc7
kkaushi <2kuldeepkaushik@gmail.com>
2014-12-21 15:24:55 -0700

    VS2013 First commit

---
+++ AllTestProjects/PaintFill.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<random>
+#include<time.h>
+using namespace std;
+
+void PrintMatrix(int**a, int nr, int nc){
+	for (auto i = 0; i < nr; i++){
+		for (auto j = 0; j < nc; j++){
+			cout << a[i][j] << "\t";
+		}
+		cout << "\n";
+	}
+}
+
+void PopulateMatrix(int **a, int nr, int nc){
+	srand(time(nullptr));
+	for (auto i = 0; i < nr; i++){
+		for (auto j = 0; j < nc; j++){
+			a[i][j] = (rand() % 2) == 0 ? 0 : 1;
+		}
+	}
+}
+
+int** RandomMatrix(int nr, int nc){
+	int **a = new int*[nr];
+	for (auto i = 0; i < nr; i++){
+		a[i] = new int[nc];
+	}
+	PopulateMatrix(a, nr, nc);
+	return a;
+}
+
+void FreeMatrix(int **a, int nr, int nc){
+	for (auto i = 0; i < nr; i++){
+		delete(a[i]); 
+	}
+	delete(a);
+}
+
+int CheckBound(int nr, int nc, int x, int y){
+	return (x < 0 || x >= nr || y < 0 || y >= nc) ? 0 : 1;
+}
+
+void Paintfill(int **a, int nr, int nc, int x, int y, int c){
+	if (!CheckBound(nr, nc, x, y) || a[x][y]==c)
+		return;
+	a[x][y] = c;
+	Paintfill(a, nr, nc, x - 1, y, c);
+	Paintfill(a, nr, nc, x + 1, y, c);
+	Paintfill(a, nr, nc, x , y - 1, c);
+	Paintfill(a, nr, nc, x , y + 1, c);
+	return;
+}
+
+
+void main(){
+	int **a, nr = 2, nc = 2;
+	cout << "Hi this is visual studio 2013! excited!\n";
+	a=RandomMatrix(nr, nc);
+	PrintMatrix(a, nr, nc);
+	Paintfill(a, nr, nc, 1, 1, 4);
+	cout << "Matrix after PaintFill() is:\n";
+	PrintMatrix(a, nr, nc);
+	FreeMatrix(a, nr, nc);
+}
